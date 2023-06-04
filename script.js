b86b6911356383237035093cf5ce46906fb2de24
Genius-Xue <62844243+Genius-Xue@users.noreply.github.com>
2023-06-04 23:07:11 +0200

    3

--- script.js
+++ script.js
@@ -1,88 +1,88 @@
-window.onload = () => {
-    const button1 = document.querySelector('button1[data-action="change"]');
-    button1.innerText = 'Scenario1';
+// window.onload = () => {
+//     const button1 = document.querySelector('button1[data-action="change"]');
+//     button1.innerText = 'Scenario1';
 
-    const button2 = document.querySelector('button2[data-action="change"]');
-    button2.innerText = 'Scenario2';
+//     const button2 = document.querySelector('button2[data-action="change"]');
+//     button2.innerText = 'Scenario2';
 
-    let places = staticLoadPlaces();
-    renderPlaces(places);
-};
+//     let places = staticLoadPlaces();
+//     renderPlaces(places);
+// };
 
-function staticLoadPlaces() {
-    return [
-        {
-            name: 'Pokèmon',
-            location: {
-                lat: 47.407440,
-                lng: 8.504920,
-            },
-        },
-    ];
-}
+// function staticLoadPlaces() {
+//     return [
+//         {
+//             name: 'Pokèmon',
+//             location: {
+//                 lat: 47.407440,
+//                 lng: 8.504920,
+//             },
+//         },
+//     ];
+// }
 
-var models = [
-    {
-        url: './assets/magnemite/scene.gltf',
-        scale: '0.5 0.5 0.5',
-        info: 'Magnemite emit electromagnetic energy from its horseshoe magnets in order to keep afloat',
-        rotation: '0 180 0',
-    },
-    {
-        url: './assets/articuno/scene.gltf',
-        scale: '0.2 0.2 0.2',
-        rotation: '0 180 0',
-        info: 'Articuno, a large avian Pokémon with predominantly sky blue plumage and wings said to be made of ice',
-    },
-    {
-        url: './assets/dragonite/scene.gltf',
-        scale: '0.08 0.08 0.08',
-        rotation: '0 180 0',
-        info: 'Dragonite, Lv. 99, HP 150/150',
-    },
-];
+// var models = [
+//     {
+//         url: './assets/magnemite/scene.gltf',
+//         scale: '0.5 0.5 0.5',
+//         info: 'Magnemite emit electromagnetic energy from its horseshoe magnets in order to keep afloat',
+//         rotation: '0 180 0',
+//     },
+//     {
+//         url: './assets/articuno/scene.gltf',
+//         scale: '0.2 0.2 0.2',
+//         rotation: '0 180 0',
+//         info: 'Articuno, a large avian Pokémon with predominantly sky blue plumage and wings said to be made of ice',
+//     },
+//     {
+//         url: './assets/dragonite/scene.gltf',
+//         scale: '0.08 0.08 0.08',
+//         rotation: '0 180 0',
+//         info: 'Dragonite, Lv. 99, HP 150/150',
+//     },
+// ];
 
-var modelIndex = 0;
-var setModel = function (model, entity) {
-    if (model.scale) {
-        entity.setAttribute('scale', model.scale);
-    }
+// var modelIndex = 0;
+// var setModel = function (model, entity) {
+//     if (model.scale) {
+//         entity.setAttribute('scale', model.scale);
+//     }
 
-    if (model.rotation) {
-        entity.setAttribute('rotation', model.rotation);
-    }
+//     if (model.rotation) {
+//         entity.setAttribute('rotation', model.rotation);
+//     }
 
-    if (model.position) {
-        entity.setAttribute('position', model.position);
-    }
+//     if (model.position) {
+//         entity.setAttribute('position', model.position);
+//     }
 
-    entity.setAttribute('gltf-model', model.url);
+//     entity.setAttribute('gltf-model', model.url);
 
-    const div = document.querySelector('.instructions');
-    div.innerText = model.info;
-};
+//     const div = document.querySelector('.instructions');
+//     div.innerText = model.info;
+// };
 
-function renderPlaces(places) {
-    let scene = document.querySelector('a-scene');
+// function renderPlaces(places) {
+//     let scene = document.querySelector('a-scene');
 
-    places.forEach((place) => {
-        let latitude = place.location.lat;
-        let longitude = place.location.lng;
+//     places.forEach((place) => {
+//         let latitude = place.location.lat;
+//         let longitude = place.location.lng;
 
-        let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+//         let model = document.createElement('a-entity');
+//         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
 
-        setModel(models[modelIndex], model);
+//         setModel(models[modelIndex], model);
 
-        model.setAttribute('animation-mixer', '');
+//         model.setAttribute('animation-mixer', '');
 
-        document.querySelector('button[data-action="change"]').addEventListener('click', function () {
-            var entity = document.querySelector('[gps-entity-place]');
-            modelIndex++;
-            var newIndex = modelIndex % models.length;
-            setModel(models[newIndex], entity);
-        });
+//         document.querySelector('button[data-action="change"]').addEventListener('click', function () {
+//             var entity = document.querySelector('[gps-entity-place]');
+//             modelIndex++;
+//             var newIndex = modelIndex % models.length;
+//             setModel(models[newIndex], entity);
+//         });
 
-        scene.appendChild(model);
-    });
-}
+//         scene.appendChild(model);
+//     });
+// }
