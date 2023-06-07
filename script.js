5f3dc377dc7e0f03914f432554291e1ef061b50e
Genius-Xue <62844243+Genius-Xue@users.noreply.github.com>
2023-06-07 23:52:41 +0200

    342

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     const button = document.querySelector('button[data-action="change"]');
-    button.innerText = 'scenario 2';
+    button.innerText = 'scenario 1';
 
     let places = staticLoadPlaces();
     renderPlaces(places);
