1ffbd46868c8e2f9fc45d3c78d721961721500fc
Genius-Xue <62844243+Genius-Xue@users.noreply.github.com>
2023-06-09 00:42:39 +0200

    1

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-document.querySelector("#button1").addEventListener("click", (e)=> {
-    document.querySelector("#anime1").setAttribute("visible", true);
-    document.querySelector("#anime2").setAttribute("visible", false);
-  })
-  
-document.querySelector("#button2").addEventListener("click", (e)=> {
-    document.querySelector("#anime1").setAttribute("visible", false);
-    document.querySelector("#anime2").setAttribute("visible", true);
-  })
 
 
-// window.onload = () => {
-
+window.onload = () => {
+    document.querySelector('#button1').addEventListener('click', function() {
+        let anime1 = document.querySelector('#anime1');
+        let anime2 = document.querySelector('#anime2');
+        anime1.setAttribute('animation-mixer', {clip: 'anime1', loop: 'repeat'});
+        anime2.removeAttribute('animation-mixer');
+      });
+      
+      document.querySelector('#button2').addEventListener('click', function() {
+        let anime1 = document.querySelector('#anime1');
+        let anime2 = document.querySelector('#anime2');
+        anime2.setAttribute('animation-mixer', {clip: 'anime2', loop: 'repeat'});
+        anime1.removeAttribute('animation-mixer');
+      });
 
 
     // const button1 = document.getElementById('button1');
@@ -35,84 +38,84 @@ document.querySelector("#button2").addEventListener("click", (e)=> {
     //     });
     //   });
 
-//     let places = staticLoadPlaces();
-//     renderPlaces(places);
-// };
+    let places = staticLoadPlaces();
+    renderPlaces(places);
+};
 
-// function staticLoadPlaces() {
-//     return [
-//         {
-//             name: 'Pokèmon',
-//             location: {
-//                 lat: 47.407440,
-//                 lng: 8.504920,
-//             },
-//         },
-//     ];
-// }
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'Pokèmon',
+            location: {
+                lat: 47.407440,
+                lng: 8.504920,
+            },
+        },
+    ];
+}
 
-// var models = [
-//     {
-//         url: './assets/magnemite/scene.gltf',
-//         scale: '0.5 0.5 0.5',
-//         info: 'Magnemite emit electromagnetic energy from its horseshoe magnets in order to keep afloat',
-//         rotation: '0 180 0',
-//     },
-//     {
-//         url: './assets/articuno/scene.gltf',
-//         scale: '0.2 0.2 0.2',
-//         rotation: '0 180 0',
-//         info: 'Articuno, a large avian Pokémon with predominantly sky blue plumage and wings said to be made of ice',
-//     },
-//     {
-//         url: './assets/dragonite/scene.gltf',
-//         scale: '0.08 0.08 0.08',
-//         rotation: '0 180 0',
-//         info: 'Dragonite, Lv. 99, HP 150/150',
-//     },
-// ];
+var models = [
+    {
+        url: './assets/magnemite/scene.gltf',
+        scale: '0.5 0.5 0.5',
+        info: 'Magnemite emit electromagnetic energy from its horseshoe magnets in order to keep afloat',
+        rotation: '0 180 0',
+    },
+    {
+        url: './assets/articuno/scene.gltf',
+        scale: '0.2 0.2 0.2',
+        rotation: '0 180 0',
+        info: 'Articuno, a large avian Pokémon with predominantly sky blue plumage and wings said to be made of ice',
+    },
+    {
+        url: './assets/dragonite/scene.gltf',
+        scale: '0.08 0.08 0.08',
+        rotation: '0 180 0',
+        info: 'Dragonite, Lv. 99, HP 150/150',
+    },
+];
 
-// var modelIndex = 0;
-// var setModel = function (model, entity) {
-//     if (model.scale) {
-//         entity.setAttribute('scale', model.scale);
-//     }
+var modelIndex = 0;
+var setModel = function (model, entity) {
+    if (model.scale) {
+        entity.setAttribute('scale', model.scale);
+    }
 
-//     if (model.rotation) {
-//         entity.setAttribute('rotation', model.rotation);
-//     }
+    if (model.rotation) {
+        entity.setAttribute('rotation', model.rotation);
+    }
 
-//     if (model.position) {
-//         entity.setAttribute('position', model.position);
-//     }
+    if (model.position) {
+        entity.setAttribute('position', model.position);
+    }
 
-//     entity.setAttribute('gltf-model', model.url);
+    entity.setAttribute('gltf-model', model.url);
 
-//     const div = document.querySelector('.instructions');
-//     div.innerText = model.info;
-// };
+    const div = document.querySelector('.instructions');
+    div.innerText = model.info;
+};
 
-// function renderPlaces(places) {
-//     let scene = document.querySelector('a-scene');
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
 
-//     places.forEach((place) => {
-//         let latitude = place.location.lat;
-//         let longitude = place.location.lng;
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
 
-//         let model = document.createElement('a-entity');
-//         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
 
-//         setModel(models[modelIndex], model);
+        setModel(models[modelIndex], model);
 
-//         model.setAttribute('animation-mixer', '');
+        model.setAttribute('animation-mixer', '');
 
-//         document.querySelector('button[data-action="change"]').addEventListener('click', function () {
-//             var entity = document.querySelector('[gps-entity-place]');
-//             modelIndex++;
-//             var newIndex = modelIndex % models.length;
-//             setModel(models[newIndex], entity);
-//         });
+        // document.querySelector('button[data-action="change"]').addEventListener('click', function () {
+        //     var entity = document.querySelector('[gps-entity-place]');
+        //     modelIndex++;
+        //     var newIndex = modelIndex % models.length;
+        //     setModel(models[newIndex], entity);
+        // });
 
-//         scene.appendChild(model);
-//     });
-// }
+        scene.appendChild(model);
+    });
+}
